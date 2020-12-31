5688c69a473c133d81c9c2098929d3b7bf6c4e3b
Carlos Estela Blanco <carlos.estela@wallbox.com>
2020-12-31 17:07:51 +0100

    artists finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,23 @@ module.exports = {
   purge: ["./src/**/*.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    height: {
+      '300': '300px',
+    },
+    maxHeight: {
+      '0': '0',
+      '1/4': '25%',
+      '1/2': '50%',
+      '3/4': '75%',
+      'full': '100%',
+    },
+    maxWidth: {
+      '70': '70%',
+      '300': '300px',
+    },
+    zIndex: {
+      'back': '-1',
+    },
     fontFamily: {
       header: ['Rubik', 'sans-serif'],
       body: ['Karla', 'sans-serif'],
